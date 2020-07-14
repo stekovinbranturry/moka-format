4c7fc2ba1a8133b28d545c1d57dceee4e611accb
Zhang Kai <zhangkai@mokahr.com>
2020-07-14 17:00:52 +0800

    feat: sort packages

--- src/sort.ts
+++ src/sort.ts
@@ -1,5 +1,16 @@
 import { window } from 'vscode';
 
+const getPath = (line: string) => line.split("from '")[1].split("'")[0];
+
+const sortByReference = (arr: string[], reference: string[]) => {
+  arr.sort((a, b) => {
+    const aPath = getPath(a);
+    const bPath = getPath(b);
+    return reference.indexOf(aPath) - reference.indexOf(bPath);
+  });
+  return arr;
+};
+
 const arryToStr = (arr: string[]) => (arr.length ? arr.join('\n') + '\n\n' : '');
 
 const removeEmptyLines = (str: string) =>
@@ -8,9 +19,42 @@ const removeEmptyLines = (str: string) =>
     .filter((line) => line)
     .join('\n');
 
-const isOthers = (str: string) => /from \'\./g.test(str);
+const isOthers = (str: string) => /from \'\./.test(str);
 
-const sortPackages = (arr: string[]) => {};
+const sortPackages = (arr: string[]) => {
+  const HEADER = [
+    'react',
+    'redux',
+    'react-redux',
+    'prop-types',
+    'react-router',
+    'react-router-dom',
+    'mage-react-router',
+  ];
+  const FOOTER = ['moka-ui', 'sugar-design', '@SDFoundation'];
+
+  let first: string[] = [];
+  let second: string[] = [];
+  let third: string[] = [];
+
+  arr.forEach((el) => {
+    const pack = getPath(el);
+    if (HEADER.includes(pack)) {
+      first.push(el);
+    } else if (FOOTER.includes(pack)) {
+      third.push(el);
+    } else {
+      second.push(el);
+    }
+  });
+
+  const secondPathList = second.map((el) => getPath(el)).sort();
+  first = sortByReference(first, HEADER);
+  second = sortByReference(second, secondPathList);
+  third = sortByReference(third, FOOTER);
+
+  return [...first, ...second, ...third];
+};
 
 const sort = (text: string) => {
   const lines = removeEmptyLines(text)
@@ -39,7 +83,7 @@ const sort = (text: string) => {
     }
   });
 
-  return arryToStr(packages) + arryToStr(others) + arryToStr(styles);
+  return arryToStr(sortPackages(packages)) + arryToStr(others) + arryToStr(styles);
 };
 
 export function sortCurrentDocument() {
@@ -48,8 +92,8 @@ export function sortCurrentDocument() {
     return;
   }
   const { document, selection } = editor;
-  const word = document.getText(selection);
-  const sortedText = sort(word);
+  const text = document.getText(selection);
+  const sortedText = sort(text);
 
   return editor.edit((edit) => edit.replace(selection, sortedText));
 }
